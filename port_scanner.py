06ca23acdd14b2de3093397e80438803cd22d180
Ahmed Almostafa <76785712+darkmedo20@users.noreply.github.com>
2023-03-02 11:55:02 +0200

    Create port_scanner.py

---
+++ port_scanner.py
@@ -0,0 +1,49 @@
+
+import nmap
+import re
+import pyfiglet
+
+# Regular Expression Pattern to recognise IPv4 addresses.
+ip_add_pattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
+# Regular Expression Pattern to extract the number of ports you want to scan. 
+# You have to specify <lowest_port_number>-<highest_port_number> (ex 10-100)
+port_range_pattern = re.compile("([0-9]+)-([0-9]+)")
+# Initialising the port numbers, will be using the variables later on.
+port_min = 0
+port_max = 65535
+
+# Basic user interface header
+banner = pyfiglet.figlet_format("DARK MEDO\nPORT SCANNER")
+print(banner)
+open_ports = []
+# Ask user to input the ip address they want to scan.
+while True:
+    ip_add_entered = input("\nPlease enter the ip address that you want to scan: ")
+    if ip_add_pattern.search(ip_add_entered):
+        print(f"{ip_add_entered} is a valid ip address")
+        break
+
+while True:
+    
+    print("Please enter the range of ports you want to scan in format: <int>-<int> (ex would be 60-120)")
+    port_range = input("Enter port range: ")
+    port_range_valid = port_range_pattern.search(port_range.replace(" ",""))
+    if port_range_valid:
+        port_min = int(port_range_valid.group(1))
+        port_max = int(port_range_valid.group(2))
+        break
+
+nm = nmap.PortScanner()
+# We're looping over all of the ports in the specified range.
+for port in range(port_min, port_max + 1):
+    try:
+        
+        result = nm.scan(ip_add_entered, str(port))
+        # Uncomment following line and look at dictionary
+        # print(result)
+        # We extract the port status from the returned object
+        port_status = (result['scan'][ip_add_entered]['tcp'][port]['state'])
+        print(f"Port {port} is {port_status}")
+    except:
+        # We cannot scan some ports and this ensures the program doesn't crash when we try to scan them.
+        print(f"Cannot scan port {port}.")
